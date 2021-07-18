f28ee367aa60f1b2252ed6f6b60fcdf807f64e23
RichardKanai <richardpoetgamer@gmail.com>
2021-07-18 15:32:23 +0300

    Gallery courasel added!

---
+++ index.js
@@ -0,0 +1,41 @@
+
+// gallery operations
+const prevbtn = document.querySelector('#prevbtn');
+const nextbtn = document.querySelector('#nextbtn');
+const ImageHolder = document.querySelector('.ImageHolder');
+const AllImages = ImageHolder.children;
+
+// next image
+function NextSlide(){
+    let currentImage = document.querySelector('.activeimg')
+    let Nextimage = currentImage.nextElementSibling;
+    if(Nextimage){
+        currentImage.classList.remove('activeimg')
+        Nextimage.classList.add('activeimg')
+    } else{
+        currentImage.classList.remove('activeimg')
+        AllImages[0].classList.add('activeimg')
+    }
+
+}
+
+
+window.onload = NextSlide();
+setInterval(NextSlide, 3000);
+nextbtn.addEventListener('click', NextSlide);
+
+
+// previous image
+function PrevSlide(){
+    let currentImage = document.querySelector('.activeimg')
+    let PrevImage = currentImage.previousElementSibling;
+    if(PrevImage){
+        currentImage.classList.remove('activeimg')
+        PrevImage.classList.add('activeimg')
+    } else{
+        currentImage.classList.remove('activeimg')
+        ImageHolder.lastElementChild.className = 'activeimg'
+    }
+}
+
+prevbtn.addEventListener('click', PrevSlide);
